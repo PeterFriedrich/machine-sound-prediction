032e4f29cb11213135b5b9bda2a6f9dcccbf4832
PeterFriedrich <peter-friedrich@hotmail.com>
2020-06-13 11:15:56 -0600

    checkpoint before pushing

--- wav-to-png-script.py
+++ wav-to-png-script.py
@@ -4,8 +4,6 @@ file, make a spectrogram of it, and save it as a png
 note: The chunk error simply means unsupported metadata at the end of the 
 wav files"""
 
-
-
 # imports
 from scipy.io import wavfile
 import scipy.io as sio
@@ -17,13 +15,9 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-# test names
-#wav_fname = "./data/file_example_WAV_1MG.wav"
-#image_fname = "test_spectrogram"
-
 
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2: # test cli args
         print("use: python script.py <wav_folder> <destination folder>")
         return 0
 
@@ -35,7 +29,7 @@ def main():
     _, _, files = next(os.walk(wav_folder))
     file_count = len(files)
 
-    # make the file to save stuff in
+    # check or make the destination folder
     if not os.path.exists(dest_folder):
         os.mkdir(dest_folder)
         print("Directory", dest_folder, "was created")
@@ -64,8 +58,7 @@ def make_save_spectro(wav_fname, image_fname):
     # make spectrogram
     freqs, time_segs, spectro_array = signal.spectrogram(data[:, 0], samplerate)
     plt.figure()
-    # need to remove tick marks and border
-    #plt.pcolormesh(time_segs, freqs, spectro_array)
+    # save image only
     plt.imsave(image_fname + ".png", spectro_array, format="png", cmap=None)
 
 if __name__ == "__main__":
